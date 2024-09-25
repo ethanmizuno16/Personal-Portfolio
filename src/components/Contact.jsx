77114559b07698e2ba00f08a9c0048a30e2972fa
ethanmizuno16 <ethanmizuno16@gmail.com>
2024-09-25 12:59:17 -0500

    linked email to allow for contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,9 @@ const Contact = () => {
         >
           {CONTACT.phoneNo}
         </motion.p>
-        <a href="#" className="border-b">{CONTACT.email}</a>
+        <a href="mailto:ethanmizuno16@gmail.com" className="border-b">
+          {CONTACT.email}
+        </a>
     </div>
     </div>
   )
